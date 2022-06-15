82baf67ea89d2bbbc43a188cdaf417cb4dc77659
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-15 14:51:41 +0300

    getting charts

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ console.log(familiya);
 var joy = "Bukhara";
 joy = "Toshkent";
 console.log(joy);*/
+
+let ism = "Doston";
+
+let familiya = "Mardonov";
+
+let fullName = ism + " " + familiya;
+
+console.log(fullName);
+
+//getting charts
+console.log(fullName[0]);
+
+//getting length
+console.log(fullName.length);
